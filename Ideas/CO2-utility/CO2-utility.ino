a458099f7e4ede7f730af147e57aec54001ece56
Slavek592 <viacheslav.nikiforov@mensagymnazium.cz>
2022-10-25 13:33:35 +0200

    Make utility code work

--- Ideas/CO2-utility/CO2-utility.ino
+++ Ideas/CO2-utility/CO2-utility.ino
@@ -33,51 +33,63 @@ byte turnOffSelfCalibrationArray[] = {
   0x00, 0x00, 0x86
 };
 
-
 char dataValue[9];
+
+int input = 0;
 
 void setup() {
   Serial2.begin(9600);
   Serial.begin(9600);
   delay(2000);
+  Serial.println("What do you want to do?");
+  Serial.println("1 -> Set zero point.");
+  Serial.println("2 -> Set span point for 2000 ppm.");
+  Serial.println("3 -> Turn off self calibration.");
+  Serial.println("4 -> Turn on self calibration.");
+  Serial.println("5 -> See readings.");
 }
 
 void loop() {
-  Serial.println("What do you want to do?");
-  Serial.println("1 -> Set zero point   2 -> Set span point for 2000 ppm    3 -> Turn off self calibration    4 -> Turn on self calibration   5 -> See readings");
-  int input = 0;
-  while (Serial.available() == 0) { input = Serial.parseInt(); }
+  if (Serial.available() > 0)
+  {
+    input = Serial.read() - 48;
+    switch (input) {
+      case 1:
+        Serial2.write(zeroPointCalibrationArray, 9);
+        Serial.println("Zero point set.");
+        break;
+      case 2:
+        Serial2.write(spanPointCalibrationArray, 9);
+        Serial.println("Span point set.");
+        break;
+      case 3:
+        Serial2.write(turnOffSelfCalibrationArray, 9);
+        Serial.println("Auto calibration off.");
+        break;
+      case 4:
+        Serial2.write(turnOnSelfCalibrationArray, 9);
+        Serial.println("Auto calibration on.");
+        break;
+      case 5:
+        Serial.println("Input anything to break.");
 
-  switch (input) {
-    case 1:
-      Serial2.write(zeroPointCalibrationArray, 9);
-      Serial.println("Zero point set");
-      break;
-    case 2:
-      Serial2.write(spanPointCalibrationArray, 9);
-      Serial.println("Span point set");
-      break;
-    case 3:
-      Serial2.write(turnOffSelfCalibrationArray, 9);
-      Serial.println("Auto calibration off");
-      break;
-    case 4:
-      Serial2.write(turnOnSelfCalibrationArray, 9);
-      Serial.println("Auto calibration on");
-      break;
-    case 5:
-      Serial.println("Input anything to break");
-
-      while (Serial.available() == 0) {
-        int co2 = 0;
-        Serial2.write(readConcentrationArray, 9);
-        Serial2.readBytes(dataValue, 9);
-        co2 = (256 * (int)dataValue[2]) + (int)dataValue[3];
-        Serial.print("CO2: ");
-        Serial.print(co2);
-        Serial.println(" ppm");
-        delay(2000);
-      }
-      break;
+        while (Serial.available() == 0) {
+          int co2 = 0;
+          Serial2.write(readConcentrationArray, 9);
+          Serial2.readBytes(dataValue, 9);
+          co2 = (256 * (int)dataValue[2]) + (int)dataValue[3];
+          Serial.print("CO2: ");
+          Serial.print(co2);
+          Serial.println(" ppm");
+          delay(2000);
+        }
+        break;
+    }
+    Serial.println("What do you want to do?");
+    Serial.println("1 -> Set zero point.");
+    Serial.println("2 -> Set span point for 2000 ppm.");
+    Serial.println("3 -> Turn off self calibration.");
+    Serial.println("4 -> Turn on self calibration.");
+    Serial.println("5 -> See readings.");
   }
 }

89ac53a393bdf9df538434556a3049d918aef656
Jan Strmiska <jan.strmiska@enerfis.cz>
2022-11-04 21:24:25 +0100

    Add pwm reading for CO2

--- SD_plantbox/SD_plantbox.ino
+++ SD_plantbox/SD_plantbox.ino
@@ -11,6 +11,7 @@ const int MHZ14_R_pin       = 8;  // CO2 sensor
 const int G328_MOS_SIG_pin  = 4;  // pump pin
 const int chipSelectSD      = 10; // Chip select for SD card reader
                                   // BME uses hard coded pins
+int MHZ14_pwm_pin           = 2;
 
 // Declare bme sensor and CO2 serial
 Adafruit_BME280 BME;
@@ -19,6 +20,8 @@ SoftwareSerial SerialCO2(MHZ14_T_pin, MHZ14_R_pin);
 // Pressure at sea level in HPa
 const float seaLevelPressure = 1013.25;
 
+// span
+const int span = 5000;
 
 void setup() {
   pinMode(G328_MOS_SIG_pin, OUTPUT);
@@ -34,8 +37,8 @@ void setup() {
   BME.begin(0x76);
 
   // Heat up CO2 sensor
-  Serial.println("Heating up CO2 sensor (50s)");
-  delay(50000);
+  Serial.println("Heating up CO2 sensor (3m)");
+  delay((unsigned long)180000);
 
   // Initialize SD card
   if (!SD.begin(chipSelectSD)) {
@@ -45,7 +48,7 @@ void setup() {
   Serial.println("card initialized.");
 
   //First line into log
-  writeLineToFile("log.txt", "time(ms),tempeature(°C),pressure(hPa),humidity(%),altitude(m),CO2(ppm)");
+  writeLineToFile("log.txt", "time(s),tempeature(°C),pressure(hPa),humidity(%),altitude(m),CO2(ppm),CO2PWM(ppm)");
 }
 
 long nextPumpTime = 0;
@@ -53,7 +56,7 @@ String dataString = "";
 void loop() {
   dataString = "";
 
-  dataString += millis();
+  dataString += (millis() / (unsigned long) 1000);
   dataString += ',';
 
   dataString += BME.readTemperature();
@@ -70,6 +73,10 @@ void loop() {
 
   dataString += getCO2();
   dataString += ',';
+
+  dataString += getCO2PWM();
+  dataString += ',';
+ 
 
   if(nextPumpTime < millis()){
     pump(12);
@@ -114,6 +121,17 @@ int getCO2(){
   
   co2 = (256 * (int)dataValue[2]) + (int)dataValue[3];
   return co2;
+}
+
+int getCO2PWM(){
+  unsigned long th, tl, ppm_pwm = 0;
+  do {
+    th = pulseIn(MHZ14_pwm_pin, HIGH, 1004000) / 1000;
+    tl = 1004 - th;
+    ppm_pwm = span * (th - 2) / (th + tl - 4);
+  } while (th == 0);
+  
+  return ppm_pwm;
 }
 
 // Pumps for x seconds

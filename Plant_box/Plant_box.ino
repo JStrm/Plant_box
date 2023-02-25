612475065a99aaeddf1d83a6e09037e63bb70f3c
Jan Strmiska <jan.strmiska@enerfis.cz>
2023-02-25 11:33:19 +0100

    stuff

--- Plant_box/Plant_box.ino
+++ Plant_box/Plant_box.ino
@@ -3,21 +3,21 @@
 #include <SPI.h>
 #include <SD.h>
 
-const byte mhz14PwmPin  = 10; // PWM pin for CO2 sensor
-const byte csSdPin      = 4;  // Chip select for SD card reader
-const byte pumpPin      = 6;  // Pump pin
-const byte ledPin       = 5;  // LED pin
+const byte mhz14PwmPin = 9;  // PWM pin for CO2 sensor
+const byte csSdPin = 4;      // Chip select for SD card reader
+const byte pumpPin = 6;      // Pump pin
+const byte ledPin = 5;       // LED pin
 
-const float seaLevelPressure  = 1013.25;
-const int co2ReadingSpan      = 5000;
-const char filename[8]        = "log.txt";
-const byte maxSdFailCount     = 100;
+const float seaLevelPressure = 1013.25;
+const int co2ReadingSpan = 5000;
+const char filename[8] = "log.txt";
+const byte maxSdFailCount = 100;
 
 
-const unsigned long co2HeatUpTime = 180000;    // 3m in ms
-const unsigned long readingPeriod = 10000;    // 10s in ms
-const unsigned long pumpPause     = 86400000; // 24h in ms
-const unsigned long pumpTime      = 60000;    // 1 min in ms
+const unsigned long co2HeatUpTime = 180000;  // 3m in ms
+const unsigned long readingPeriod = 10000;   // 10s in ms
+const unsigned long pumpPause = 86400000;    // 24h in ms
+const unsigned long pumpTime = 60000;        // 1 min in ms
 
 
 Adafruit_BME280 BME;
@@ -36,19 +36,19 @@ void setup() {
     somethingIsWrong();
   }
 
-  writeLineToFile(F("time(s),temperature(°C),pressure(hPa),humidity(%),altitude(m),CO2PWM(ppm),"));
+  writeLineToFile(F("\ntime(s),temperature(°C),pressure(hPa),humidity(%),altitude(m),CO2PWM(ppm),"));
 }
 
-unsigned long nextPumpTime  = 10000;
-unsigned long nextReading   = 0;
-bool pumpOn                 = false;
-const char separator[3]     = ", ";
+unsigned long nextPumpTime = 10000;
+unsigned long nextReading = 0;
+bool pumpOn = false;
+const char separator[3] = ", ";
 
 void loop() {
   if (nextReading < millis()) {
     digitalWrite(ledPin, HIGH);
-  
-    writeToFile(String(millis() / 10000));
+
+    writeToFile(String(millis() / 1000));
     writeToFile(separator);
     writeToFile(String(BME.readTemperature()));
     writeToFile(separator);
@@ -69,13 +69,13 @@ void loop() {
 
   if ((nextPumpTime < millis()) && !pumpOn) {
     digitalWrite(pumpPin, HIGH);
-    nextPumpTime += pumpTime;  
+    nextPumpTime += pumpTime;
     pumpOn = true;
   }
 
   if ((nextPumpTime < millis()) && pumpOn) {
     digitalWrite(pumpPin, LOW);
-    nextPumpTime += pumpPause;  
+    nextPumpTime += pumpPause;
     pumpOn = false;
   }
 }
@@ -109,21 +109,23 @@ void writeToFile(String string) {
   }
 }
 
-unsigned long pwmCo2Concentration() {
-  unsigned long th, tl, ppm_pwm = 0;
-  do {
-    th = pulseIn(mhz14PwmPin, HIGH, 1004000) / 1000;
-    tl = 1004 - th;
-    ppm_pwm = co2ReadingSpan * (th - 2) / (th + tl - 4);
-  } while (th == 0);
-
-  return ppm_pwm;
+int pwmCo2Concentration() {
+  while (digitalRead(mhz14PwmPin) == LOW) {};
+  long t0 = millis();
+  while (digitalRead(mhz14PwmPin) == HIGH) {};
+  long t1 = millis();
+  while (digitalRead(mhz14PwmPin) == LOW) {};
+  long t2 = millis();
+  long tH = t1 - t0;
+  long tL = t2 - t1;
+  long ppm = co2ReadingSpan * (tH - 2) / (tH + tL - 4);
+  return int(ppm);
 }
 
 void somethingIsWrong() {
   while (true) {
     digitalWrite(ledPin, HIGH);
-    delay(400);
+    delay(100);
     digitalWrite(ledPin, LOW);
     delay(100);
   }
